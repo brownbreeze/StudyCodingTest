68706d692e776395a6b67d511c3e2b810c11da12
brwonbreeze <913breeze@gmail.com>
2023-01-04 00:12:02 +0900

    [daily]python:programmers
    - level 2
    
    issue : #1

--- programmers/2_영어_끝말잇기/source_20230103.py
+++ programmers/2_영어_끝말잇기/source_20230103.py
@@ -6,17 +6,16 @@ def solution(n, words):
     last_alpha = ''
     while i<len_words:
         n_idx = int(i % n)
-        w_idx = int(i % len_words)
-        if words[w_idx] in n_set:
-            return [n_idx+1, (i+1)//n]
-        n_set.add(words[w_idx])
+        if words[i] in n_set:
+            return [n_idx+1, (i+1)//n if (i+1)%n==0 else (i+1)//n+1]
+        n_set.add(words[i])
         if last_alpha == '':
-            last_alpha = words[w_idx][-1]
+            last_alpha = words[i][-1]
             i+=1
             continue
-        if last_alpha != words[w_idx][0]:
-            i+=1
-            return [n_idx+1, (i+1)//n]
-        last_alpha = words[w_idx][-1]
+        if last_alpha != words[i][0]:
+            #i+=1
+            return [n_idx+1, (i+1)//n if (i+1)%n==0 else (i+1)//n+1]
+        last_alpha = words[i][-1]
         i+=1
     return [0, 0]

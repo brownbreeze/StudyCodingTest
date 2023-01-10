af0295f0d21c308910480a7a735494bb551ec7aa
brwonbreeze <913breeze@gmail.com>
2023-01-10 23:58:51 +0900

    [daily]python:programmers
    - level 2
    
    issue : #1

--- programmers/2_이진_변환_반복하기/source_20230110.py
+++ programmers/2_이진_변환_반복하기/source_20230110.py
@@ -1,8 +1,7 @@
 def go_func(s):
     new_s = list(s)
     c = new_s.count('1')
-    print(len(list(s))-len(new_s))
-    return len(s)-len(new_s), "{0:b}".format(c)
+    return new_s.count('0'), "{0:b}".format(c)
     
 def solution(s):
     answer = [0,0]
@@ -10,7 +9,7 @@ def solution(s):
         del_num, new_s = go_func(s)
         answer[1] += del_num
         answer[0] += 1
-        print(new_s, del_num)
+        # print(new_s, del_num)
         s = new_s
-        if answer[0] == 2 : break
+        if s == '1':break
     return answer

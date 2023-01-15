9ccde9d8db71fa45558b012b4906e3883ef464ce
brwonbreeze <913breeze@gmail.com>
2023-01-15 20:48:29 +0900

    [daily]python:programmers
    - level 2
    - not greedy , 완전탐색
    
    issue : #1

--- programmers/2_조이스틱/source_20230115.py
+++ programmers/2_조이스틱/source_20230115.py
@@ -1,23 +1,25 @@
+def go_find(lst, cnt, loc):
+    cnt += lst[loc]
+    temp = lst[loc]
+    lst[loc] = 0
+    if sum(lst) == 0:
+        lst[loc] = temp
+        return cnt
+    left = 1
+    right = 1
+    while lst[loc + right] == 0:
+        right += 1
+    while lst[loc-left] == 0:
+        left += 1
+    a = go_find(lst, cnt + right, loc+right)
+    b = go_find(lst, cnt + left, loc-left)
+    lst[loc] = temp
+    return a  if a < b else b
+
 def solution(name):
     lst = [min(ord(n) - ord('A'), ord('Z')- ord(n)+1) for n in name]
     #print(lst)
     cnt = 0 
     loc = 0
-    while True:
-        cnt += lst[loc]
-        lst[loc] = 0
-        if sum(lst) == 0:
-            break
-        left = 1
-        right = 1
-        while lst[loc + right] == 0:
-            right += 1
-        while lst[loc-left] == 0:
-            left += 1
-        if left >= right :
-            loc += right
-            cnt += right
-        else :
-            loc -= left
-            cnt += left
-    return cnt
+    answer = go_find(lst, cnt, loc)
+    return answer

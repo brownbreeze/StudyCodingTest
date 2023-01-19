f0f231a335597b3f517a388e2af4938a9e8d42b7
brwonbreeze <913breeze@gmail.com>
2023-01-19 23:46:43 +0900

    [daily]python:programmers
    - level 2
    
    issue : #1

--- programmers/2_짝지어_제거하기/source_20230119.py
+++ programmers/2_짝지어_제거하기/source_20230119.py
@@ -1,20 +1,12 @@
 def solution(s):
-    answer = -1
-    pos = 0
+    answer = []
     arr = list(s)
-    flag = 0
-    while True:
-        if pos+1 >= len(arr):
-            if flag == 0 : break 
-            flag = 0
-            pos = 0
-        if arr[pos]==arr[pos+1]:
-            # print(f'arr:{arr}, pos:{pos}')
-            arr.pop(pos)
-            arr.pop(pos)
-            flag = 1
-        else:
-            pos += 1
-        if len(arr) == 0 or len(arr) == 1: 
-            break
-    return 1 if len(arr)==0 else 0
+    for alpha in arr:
+        if len(answer) == 0 :
+            answer.append(alpha)
+            continue
+        if answer[-1] == alpha:
+            answer.pop()
+            continue
+        answer.append(alpha)
+    return 1 if len(answer)==0 else 0

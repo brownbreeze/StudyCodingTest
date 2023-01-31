afa1c4c017081afd384559e3a1283c27f8a44ce1
brwonbreeze <913breeze@gmail.com>
2023-01-31 22:25:47 +0900

    [daily]python:programmers
    - level 2
    - bit
    
    issue : #1

--- programmers/2_2개_이하로_다른_비트/source_20230131.py
+++ programmers/2_2개_이하로_다른_비트/source_20230131.py
@@ -5,12 +5,14 @@ def solution(numbers):
         if number in history:
             answer.append(history[number])
             continue
-        b = number
-        while True:
-            b+=1
-            diff = list("{0:b}".format(number^b)).count('1')
-            if diff <= 2 :
-                break
-        answer.append(b)
-        history[number] = b
+        data = "{0:b}".format(number)
+        idx = 0 
+        for i in data[::-1]:
+            idx +=1
+            if i == '0' : break
+        else:
+            idx+=1
+        temp = 1 if idx == 1 else pow(2,idx-2)
+        answer.append(number+temp)
+        history[number] = number+temp
     return answer

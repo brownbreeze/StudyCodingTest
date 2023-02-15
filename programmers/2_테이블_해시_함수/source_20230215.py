f8466b928f8bbfd4a37f6caacdfe066c25aa95a7
brwonbreeze <913breeze@gmail.com>
2023-02-15 23:23:49 +0900

    [daily]python:programmers
    - level 2
    - retry
    - sort
    
    issue : #1

---
+++ programmers/2_테이블_해시_함수/source_20230215.py
@@ -0,0 +1,13 @@
+def solution(data, col, row_begin, row_end):
+    answer = -1
+    data.sort(key=lambda x:(x[col-1],-x[0]))
+    s_i = 0 
+    temp = -1
+    for i in range(row_begin-1, row_end):            
+        s_i = 0 
+        for d in data[i]:
+            s_i += d%(i+1)
+        if temp != -1:
+            answer = s_i^temp
+        temp = s_i
+    return answer

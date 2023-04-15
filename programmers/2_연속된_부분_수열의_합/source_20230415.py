e9ed6cffa768fdfdcec905031702e5bdbb936db7
brwonbreeze <913breeze@gmail.com>
2023-04-15 23:40:38 +0900

    [daily]python:programmers
    - level 2
    - not use `sum()` every turn

--- programmers/2_연속된_부분_수열의_합/source_20230415.py
+++ programmers/2_연속된_부분_수열의_합/source_20230415.py
@@ -3,22 +3,35 @@ def solution(sequence, k):
     answer = []
     result_list = []
     i = 0
-    j = 1
+    j = 0
+    total = sequence[0]
     while True:
-        if j<i or len(sequence)<j: 
-            # print('brak', i, j, sum(sequence[i:j]))
+        if j<i or len(sequence) < j: 
+            # print('brak', i, j, total)
             break
-        if k > sum(sequence[i:j]):
-            # print('j++', i, j, sum(sequence[i:j]))
+        if k > total:
+            # print('j++', i, j, total)
             j+=1
+            if j >= len(sequence): 
+                # print('err')
+                break
+            total += sequence[j]
             continue
-        if k < sum(sequence[i:j]):
-            # print('i++', i, j, sum(sequence[i:j]))
+        if k < total:
+            # print('i++', i, j, total)
+            total -= sequence[i]
             i+=1
             continue
-        result_list.append([i,j-1,j-i])
-        i+=1
+        # print('correct', i, j, total)
+        result_list.append([i,j,j-i])
         j+=1
+        if j >= len(sequence): 
+            # print('err')
+            break
+        total += sequence[j]
+        total -= sequence[i]
+        i+=1
+    # print(result_list)
     result_list = sorted(result_list, key=lambda x:(x[2], x[0]))
     if len(result_list) == 0 :
         return [0,0]

e1a3658ec60c67ea122951ecf042fe56bc9bea24
brownbreeze <913breeze@gmail.com>
2023-08-08 07:50:32 -0700

    [daily]python:codility
    - easy PermMissingElem
    - failed

--- codility/lesson_3_PermMissingElem/solution.py
+++ codility/lesson_3_PermMissingElem/solution.py
@@ -1,1 +1,13 @@
-source code
+def solution(A):
+    A = sorted(A)
+    left = 0
+    right = len(A) -1
+    while True:
+        idx = left
+        if left > right:
+            return idx+1
+        if A[idx] == idx + 1 :
+            left = (right - left)//2 + left + 1
+        elif A[idx] != idx + 1 :
+            right = (right-left)//2 + left - 1
+    return len(A)+1

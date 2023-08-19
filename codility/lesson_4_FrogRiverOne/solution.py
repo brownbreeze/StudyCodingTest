b681bdc4955797c7b3aa76fa90065085e54d4a73
brownbreeze <913breeze@gmail.com>
2023-08-19 06:34:40 -0700

    [daily]python:codility
    - easy FrogRiverOne
    - 빠진 부분 재커밋

--- codility/lesson_4_FrogRiverOne/solution.py
+++ codility/lesson_4_FrogRiverOne/solution.py
@@ -1,1 +1,12 @@
-source code
+def solution(X, A):
+    arr = [1 for _ in range(0,1000001)]
+    tmp = 0
+    i = 0
+    for a in A:
+        if arr[a]==1 and a<=X:
+            arr[a] = 0
+            tmp += 1
+        if tmp == X:
+            return i
+        i += 1
+    return i 

0782b16794230a5f9ff7892a6880145edd7d1e58
brownbreeze <913breeze@gmail.com>
2023-09-16 07:01:32 -0700

    [daily]python:codility
    - easy fish
    - 0... 처음

---
+++ codility/lesson_7_noNamed/solution_2.py
@@ -0,0 +1,22 @@
+def solution(A, B):
+    len_a = len(A)
+    stack = []
+    big_size = 0
+    for i in range(len_a):
+        if B[i] == 0 : 
+            stack.append(A[i])
+            while True:
+                if len(stack) == 0: break
+                if stack[-1] < big_size: stack.pop()
+                else : break
+            
+        else:
+            if big_size == 0 : 
+                big_size = A[i]
+                # stack.append(A[i])
+            else: 
+                if big_size > A[i]:
+                    continue
+        # print(stack) 
+    # print(stack)
+    return len(stack)

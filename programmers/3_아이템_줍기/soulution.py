75dd1f4a611c6bf910b5d1b165360a97b6f7fa65
brownbreeze <913breeze@gmail.com>
2023-10-28 08:08:52 -0700

    [daily]python:programmers
    - 아이템 줍기
    - level 3
    - 79.3 /100 ...

--- programmers/3_아이템_줍기/soulution.py
+++ programmers/3_아이템_줍기/soulution.py
@@ -1,59 +1,51 @@
 from collections import deque
 
-def get_dir(y, x, d, left_zero, maps):
-    go_pos = [[-1,0,1,0],[0,-1,0,1]]
-    i = d
-    fy = y + go_pos[0][i]
-    fx = x + go_pos[1][i]
-    ry = y + go_pos[0][(i+1)%4]
-    rx = x + go_pos[1][(i+1)%4]
-    ly = y + go_pos[0][(i+3)%4]
-    lx = x + go_pos[1][(i+3)%4]
+def check_can_go(maps, y, x, d):
+    go_pos = [[-1,0,1,0], [0,-1,0,1]]
+    ny = y + go_pos[0][d]
+    nx = x + go_pos[1][d]
+    if ny not in range(0,102) or nx not in range(0,102) : return False
+    if maps[ny][nx] == 9: 
+        return True
+    return False
 
-    if maps[ry][rx] + maps[ly][lx] + maps[fy][fx] == 3:
-        if left_zero :
-            return (i+3)%4
-        else :
-            return (i+1)%4
-    elif maps[ry][rx] == 1 and maps[ly][lx]==0 and maps[ry][rx] == 0 :
-        return i
-    elif maps[ry][rx] == 0:
-        if maps[ry][rx] == 1:
-            return (i+1)%4
-        elif maps[ly][lx] == 1:
-            return (i+3)%4            
-    return -1
-            
 def bfs(maps, arr, toy, tox):
     go_pos = [[-1,0,1,0],[0,-1,0,1]]
     while True:
         if len(arr) == 0 : break
-        data = arr.pop()
-        d = get_dir(data[0], data[1], data[2], data[3], maps)
-        print(data,'d', d)
-        if d == -1 : 
-            print(data[2])
-            continue 
-        ny = data[0] + go_pos[0][d]
-        nx = data[1] + go_pos[1][d]
-        if ny == toy and nx == tox : 
-            return data[3]+1
-        arr.append([ny, nx, d, data[3], data[4]+1])
+        data = arr.pop() # cnt, y, x, dir      
+        if data[1] == toy and data[2] == tox: return data[0]
+        for i in range(4):
+            if (i+2)%4 == data[3] : continue
+            if check_can_go(maps, data[1], data[2], i):
+                arr.append([data[0]+1, data[1]+go_pos[0][i], data[2]+go_pos[1][i], i])
+    
     return -1
 
 def solution(rectangle, characterX, characterY, itemX, itemY):
-    
-    maps =[[0 for _ in range(50)] for _ in range(50)]
+    maps =[[0 for _ in range(51*2)] for _ in range(51*2)]
     go_pos = [[-1,0,1,0],[0,-1,0,1]]
     for rec in rectangle:
         lx, ly, rx, ry = rec
-        for y in range(ly, ry+1):
-            for x in range(lx, rx+1):
-                maps[y][x] = 1
-    arr = deque()
+        for y in range(ly*2, (ry+1)*2-1):
+            for x in range(lx*2, (rx+1)*2-1):
+                if maps[y][x] == 0 or  maps[y][x] == 9 :
+                    if lx*2 == x or ly*2 == y or ry*2 == y or rx*2 == x:
+                        maps[y][x] = 9
+                    else:
+                        maps[y][x] = 2
+    total_len = 0
+    for y in range(0, 102):
+        for x in range(0, 102):
+            total_len = total_len+1 if maps[y][x]==9 else total_len
+
+    arr = deque()      
     for i in range(4):
-        
-        lef_y = characterY + go_pos[0][(i+3)%4]
-        lef_x = characterX + go_pos[1][(i+3)%4]
-        arr.append([characterY, characterX, i, (maps[lef_y][lef_x]==0), 0])
-    return bfs(maps, arr, itemY, itemX)
+        zeroy = characterY*2 + go_pos[0][i]
+        zerox = characterX*2 + go_pos[1][i]
+        if maps[zeroy][zerox] == 0:
+            arr.append([0, characterY*2, characterX*2, (i+1)%4]) 
+            break
+    result = bfs(maps, arr, itemY*2, itemX*2)
+    result = total_len - result if total_len - result < result else result
+    return result//2

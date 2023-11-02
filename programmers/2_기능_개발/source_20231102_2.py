bb3027b4f844a82d9b9c41de92b3e0cf4dea6204
brownbreeze <913breeze@gmail.com>
2023-11-02 22:24:14 +0900

    [daily]python:programmers
    - 기능 개발
    - level 2
    - success~ 옛날문제 두번째 풀기~

---
+++ programmers/2_기능_개발/source_20231102_2.py
@@ -0,0 +1,14 @@
+import math
+def solution(progresses, speeds):
+    answer = []
+    len_total = len(progresses)
+    days = 0
+    for i in range(len_total):
+        progress = progresses[i]
+        speed = speeds[i]
+        if progress + days * speed < 100:
+            days = days + math.ceil((100-(progress+days*speed))/speed)
+            answer.append(1)
+        else : 
+            answer[-1] +=1
+    return answer

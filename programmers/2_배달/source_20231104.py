52ae57274d72c830ab846bb9c80183c5195347bb
brownbreeze <913breeze@gmail.com>
2023-11-04 22:09:46 +0900

    [daily]python:programmers
    - 배달
    - level 2
    - time over

---
+++ programmers/2_배달/source_20231104.py
@@ -0,0 +1,37 @@
+from collections import deque 
+
+def solution(N, road, K):
+    answer = 0
+    global time_to_go
+    time_to_go = [[100000 for _ in range(N)] for _ in range(N)]
+    result = [0 for _ in range(N)]
+    stack = deque()
+    for r in road:
+        f = min(r[0], r[1])
+        t = max(r[1], r[0])
+        if f == 1:
+            stack.append([[f-1,t-1],r[2]]) # list(이미 방문), 거리값
+    while True:
+        if len(stack) == 0 : break
+        gone_list, value = stack.popleft()
+        last_gone = gone_list[-1]
+        if result[last_gone] == 0 : 
+            result[last_gone] = value
+        else:
+            result[last_gone] = min(result[last_gone], value)
+        for r in road:
+            if r[0] -1 == last_gone :
+                if r[1] -1 in gone_list : continue 
+                gone_list.append(r[1]-1)
+                stack.append([gone_list.copy(), r[2]+value])
+                gone_list.pop()
+            if r[1] -1 == last_gone :
+                if r[0] -1 in gone_list : continue 
+                gone_list.append(r[0]-1)
+                stack.append([gone_list.copy(), r[2]+value])
+                gone_list.pop()
+    # print(result)
+    for i in result:
+        if i <= K:
+            answer += 1
+    return answer

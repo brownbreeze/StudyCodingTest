0566e792b5ff752c350b1bcf9bbbf54084741fdf
brownbreeze <913breeze@gmail.com>
2024-01-08 22:57:41 +0900

    [daily]python:programmers
    - level 2
    - 마법의 엘리베이터

--- programmers/2_마법의_엘리베이터/source.py
+++ programmers/2_마법의_엘리베이터/source.py
@@ -1,29 +1,31 @@
 from collections import deque 
 
-def solution(storey):
-    answer = 0
-    check = [0 for _ in range(100000000)]
-    # BFS
-    arr = deque()
-    vals = [-10000000,-1000000,-100000,-10000,-1000,-100,-10,-1,1,10,100,1000,10000,100000,1000000, 10000000]
+def get_vals(num):
+    val = [int(i) for i in str(num)]
+    val = val[::-1]
+    i = 0
+    for i in range(len(val)):
+        if val[i] != 0 :
+            break
     
-    for val in vals:
-        if storey+val >= 0 and pow(10, len(str(storey))) > storey+val:
-            arr.append([1, storey+val])
-            check[storey+val] = 1
+    return [(10-val[i]), pow(10, i) *  (10-val[i])+ num], [val[i], (pow(10, i) * val[i]  * -1) + num]
+    
+def solution(storey):
+    answer = 100000000
+    lia, lib = get_vals(storey)
+    dq = deque() 
+    dq.append(lia)
+    dq.append(lib)
     
     while True:
-        if len(arr)== 0 :
-            break
-        temp = arr.popleft()
-        idx = temp[0]
-        cur = temp[1]
-        if cur == 0: return idx
-    
-        for val in vals:
-            t = cur+val
-            if t >= 0 and t in range(100000000) and check[cur+val]==0 and  pow(10, len(str(storey))) > t:
-                check[cur+val] = 1
-                arr.append([idx+1, cur+val])
-        if idx >= 20: break
+        if len(dq)==0: break
+        cnt, num = dq.popleft()
+        if cnt >= answer :
+            continue
+        if num == 0:
+            answer = min(answer, cnt)
+        lia, lib = get_vals(num)
+        dq.append([lia[0]+cnt, lia[1]])
+        dq.append([lib[0]+cnt, lib[1]])
+        
     return answer

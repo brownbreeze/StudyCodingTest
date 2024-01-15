e516fc42852da955a64cd20aae0d824ecd546bd5
brownbreeze <913breeze@gmail.com>
2024-01-15 23:25:32 +0900

    [daily]python:programmers
    - level 2
    - fail.. 미로탈출

--- programmers/2_미로_탈출/source.py
+++ programmers/2_미로_탈출/source.py
@@ -5,57 +5,62 @@ class Pos:
         self.y = b
     def __repr__(self):
         return f'{self.y}, {self.x}'
+    
+dir_pos = [[-1,0,1,0], [0,-1,0,1]]
+visited = list()
 
-def get_path(maps, rc, cc, pos_f, pos_t):
-    dir_pos = [[-1,0,1,0], [0,-1,0,1]]
+def get_path(maps, rc, cc, pos_f, pos_t):    
     have_to = deque()
-    maps[pos_f.y][pos_f.x] = 19
-    have_to.append([pos_f,maps.copy(), 0])    
+    have_to.append([pos_f, 0])   
+    # global visited
     
     while have_to:
-        p, m, c = have_to.popleft()
-        # print(p,'->',c, len(have_to), m)
+        p, c = have_to.popleft()
+        print(c,'----', p, '---',len(have_to))
+        if visited[p.y*cc+p.x] == 1:
+            continue 
+        visited[p.y*cc +p.x] = 1
+        
         if p.y == pos_t.y and p.x == pos_t.x:
             return c
         for i in range(4):
             new_y = p.y+dir_pos[0][i]
             new_x = p.x+dir_pos[1][i]
-            if new_y in range(rc) and new_x in range(cc):
-                if m[new_y][new_x] ==19 :
+            print('> ', new_y,new_x)
+            if new_y >= 0 and new_y < rc and new_x >= 0 and new_x < cc:
+                if maps[new_y*cc+new_x] ==19 :
+                    continue
+                elif visited[new_y*cc+new_x]==1:
                     continue
             else:
                 continue
-            m[new_y][new_x] = 19
-            have_to.append([Pos(new_y, new_x), m.copy(), c+1])
-            m[new_y][new_x] = 10
+            have_to.append([Pos(new_y, new_x), c+1])
+
             
     # print('fail')
     return -1        
     
     
 def solution(maps):
+    val = dict()
+    global visited
     rowcnt = len(maps)
     colcnt = len(maps[0])
     
-    val = dict()
-    num_map = []
-
+    num_map = [0 for _ in range(rowcnt*colcnt)]
+    visited = [0 for _ in range(rowcnt*colcnt)]
+    
     for y in range(rowcnt):
-        temp = []
         for x in range(colcnt):
             if maps[y][x] in ['S', 'L', 'E']: 
                 val[maps[y][x]] = Pos(x,y)
-            temp.append(ord(maps[y][x])-69) # 14-> S, 10->O, 7->L, 19 ->X, 0-> E
-        num_map.append(temp)
-    # print(val)
-    # print(num_map)
-    # return -1
+            num_map[y*colcnt+x] = ord(maps[y][x])-69 # 14-> S, 10->O, 7->L, 19 ->X, 0-> E
+
     answer = get_path(num_map, rowcnt,colcnt,val['S'], val['L'])
-    print(answer)
     if answer == -1:
         return answer
-    answer2 = get_path(num_map, rowcnt, colcnt, val['L'], val['E'])
-    print(answer2)
-    if answer2 == -1:
-        return -1
-    return answer+answer2+1
+    # visited = [0 for _ in range(rowcnt*colcnt+1)]
+    # answer2 = get_path(num_map, rowcnt, colcnt, val['L'], val['E'])
+    # if answer2 == -1:
+    return -1
+    # return answer+answer2+1

2834d3457120a19cf21ce280b7fbce14acaf0333
brownbreeze <913breeze@gmail.com>
2024-01-30 00:08:29 +0900

    [daily]python:programmers
    - level 2
    - 요격 시스템
      - 완전탐색 -> 시간초과

--- programmers/2_요격_시스템/source.py
+++ programmers/2_요격_시스템/source.py
@@ -1,25 +1,23 @@
 from itertools import combinations
 def solution(targets):
-    answer = 0
-    d = {}
+    values = {}
     i = 0
-    tl = len(targets)
-    values = [i for i in range(1,tl+1)]
-    for a,b in targets:
-        for x in range(a,b+1):
-            if x in d:
-                d[x].append(i)
+    for s,e in targets:
+        for x in range(s,e):
+            if x in values:
+                values[x].append(i)
             else:
-                d[x] = [i]
-        i+=1
-    keys = sorted(list(d.keys()))
-    max_key = keys[-1]
-    for i in range(1,max_key+1):
-        checks = list(combinations(keys, i))
-        for check in checks:
-            temp = set()
-            for j in list(check):
-                temp = temp | set(d[j])
-            if len(temp) == tl:
+                values[x] = [i]
+        i += 1
+    tl = len(targets)
+    keys = values.keys()
+    kl = len(keys)
+    for i in range(1, kl+1):
+        c = combinations(keys, i)
+        for l in c:
+            val = set()
+            for x in l:
+                val = val | set(values[x])
+            if len(val) == tl:
                 return i
-    return answer
+    return -1

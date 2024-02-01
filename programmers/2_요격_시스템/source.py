051adb43f56c5f0d53aa15481a009b8561c09fdd
brownbreeze <913breeze@gmail.com>
2024-02-01 23:20:00 +0900

    [daily]python:programmers
    - level 2
    - 요격 시스템..

--- programmers/2_요격_시스템/source.py
+++ programmers/2_요격_시스템/source.py
@@ -1,23 +1,11 @@
-from itertools import combinations
 def solution(targets):
-    values = {}
-    i = 0
-    for s,e in targets:
-        for x in range(s,e):
-            if x in values:
-                values[x].append(i)
-            else:
-                values[x] = [i]
-        i += 1
-    tl = len(targets)
-    keys = values.keys()
-    kl = len(keys)
-    for i in range(1, kl+1):
-        c = combinations(keys, i)
-        for l in c:
-            val = set()
-            for x in l:
-                val = val | set(values[x])
-            if len(val) == tl:
-                return i
-    return -1
+    answer = 0
+    targets.sort(key = lambda x: [x[1], x[0]])
+    
+    s = e = 0
+    for target in targets:
+        if target[0] >= e:
+            answer += 1
+            e = target[1]
+
+    return answer 

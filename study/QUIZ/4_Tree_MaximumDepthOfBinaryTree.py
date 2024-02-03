d11b29fc04d9a0c4076ca86ad28a60b4ac89ccb8
brownbreeze <913breeze@gmail.com>
2024-02-03 20:32:50 +0900

    [daily]basic
    - tree MaximumDepthOfBinaryTree

---
+++ study/QUIZ/4_Tree_MaximumDepthOfBinaryTree.py
@@ -0,0 +1,22 @@
+class Solution:
+    def lowestCommonAncestor(
+        self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
+    ) -> "TreeNode":
+        if root == None:
+            return None
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if root.val == p.val or root.val == q.val:
+            return root
+        elif left and right:
+            return root
+				else:
+		        return left or right
+        # elif left:
+        #     return left
+        # elif right:
+        #     return right
+        # else:
+        #      reutrn None

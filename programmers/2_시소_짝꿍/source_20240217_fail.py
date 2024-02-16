47f9acb481de68312bb95b602111d1e98c09acf7
brownbreeze <913breeze@gmail.com>
2024-02-17 03:03:11 +0900

    [daily]python:programmers
    - level 2
    - 시소 짝꿍 fail .. retry..

--- programmers/2_시소_짝꿍/source_20240217_fail.py
+++ programmers/2_시소_짝꿍/source_20240217_fail.py
@@ -2,16 +2,26 @@ from itertools import combinations
 def solution(weights):
     answer = 0
     li = combinations(weights, 2)
-
+    datas = {}
+    positions = [(1,1),(2, 3), (2, 4), (3, 4), (4, 3), (4, 2), (3, 2)]
+    
     for a,b in li:
         flag = 0
-        # print(a,b)
-        for i in range(2,5):
-            if flag: 
+        if a*1000+b in datas :
+            answer += datas[a*1000+b]
+            continue 
+        if b*1000+a in datas :
+            answer += datas[b*1000+a]
+            continue 
+        if a == b:
+            answer +=1
+            datas[a*1000+b] = 1
+            continue
+        for i,j in positions:
+            if a*i == b*j:
+                answer +=1
+                flag = 1
                 break
-            for j in range(2,5):
-                if a*i == b*j:
-                    answer +=1
-                    flag = 1
+        datas[a*1000+b] = flag
 
     return answer

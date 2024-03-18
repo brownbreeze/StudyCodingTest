485c0c5a21a4f43b1d67ee3745f30a4cf7e328b6
brownbreeze <913breeze@gmail.com>
2024-03-19 00:23:36 +0900

    [daily]python:backjoon
    - base 수정

--- backjoon/base/source.py
+++ backjoon/base/source.py
@@ -1,8 +1,15 @@
 import sys
+
+
 def solution(num):
-	return str(bin(num)).count('1')
+    return str(bin(num)).count('1')
+
 
 input = sys.stdin.readline
 t = int(input())
 print(solution(t))
 
+# n = int(sys.stdin.readline())
+# while n != 0:
+#     solution(n)
+#     n = int(sys.stdin.readline())

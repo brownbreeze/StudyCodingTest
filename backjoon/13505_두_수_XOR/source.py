363f98b170f39d186721fa1b6572347dd385966b
brownbreeze <913breeze@gmail.com>
2024-03-27 22:39:51 +0900

    [daily]python:backjoon
    - 13505 fail

--- backjoon/13505_두_수_XOR/source.py
+++ backjoon/13505_두_수_XOR/source.py
@@ -1,20 +1,15 @@
 import sys
+import itertools
 
 
 def solution(num, arr):
-    arr = set(arr)
-    arr = list(arr)
-    if len(arr) <= 1:
-        return 0
-    num = len(arr)
     result = 0
     arr.sort()
-    for i in range(num//2+1):
-        for j in range(num-1, i, -1):
-            if result == 0:
-                result = arr[i] ^ arr[j]
-            elif result < arr[i] ^ arr[j]:
-                return arr[i] ^ arr[j]
+    num = len(arr)
+    for i in range(num):
+        for j in range(i+1, num):
+            v = arr[i] ^ arr[j]
+            result = max(v, result)
     return result
 
 

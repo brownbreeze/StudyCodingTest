0a33efb73415d4ee1d75eeadac6fc53d2e8c47f2
brownbreeze <913breeze@gmail.com>
2024-04-10 11:22:43 +0900

    [daily]python:programmers
    - level 3
    - 거스름돈

---
+++ programmers/3_거스름돈/source_20240410.py
@@ -0,0 +1,16 @@
+def solution(n, money):
+    answer = 0
+    money.sort()
+    while True:
+        if money[-1] > n:
+            money.pop()
+        else:
+            break
+
+    dp = [1] + [0] * n
+    for m in money:
+        for i in range(m, n+1):
+            dp[i] += dp[i-m]
+    print(dp)
+    print(n, money)
+    return dp[n % 1000000007]

5acc22a5746d863afd78a873ac5e807d41137dd9
brownbreeze <913breeze@gmail.com>
2024-05-19 00:44:42 +0900

    [daily]python:programmers
    - level 3
    - 기지국 설치
    - fail.. 하지만 timeover 문제 해결
        - 기준이 N이 아닌 Stations

--- programmers/3_기지국_설치/source_20240519.py
+++ programmers/3_기지국_설치/source_20240519.py
@@ -1,29 +1,25 @@
+import math
 def solution(n, stations, w):
     answer = 0
-    # apart_arr = [0 for _ in range(n+1)]
-    if n == 0 : 
-        return 0
-    
-    # aprt_arr setting 
-    j = 0 
-    # for i in range(n):
-    #     if i in range(stations[j]-w,stations[j]+w+1):
-    #         apart_arr[i] = 1
-    #     if stations[j]-1+w<i:
-    #         j+=1
-    #     if j >= len(stations):
-    #         break 
-    
-    i = 0
-    c = 0
-    while True:
-        if i > n : break 
-        # if apart_arr[i] == 1:
-        #     i+=1
-        #     continue     
-        c += 1
-        i+=1
-        # i += w*2+1
-        # if i > n:
-        #     return c
-    return c
+    if n == 0: return 0
+    i = 0 
+    wl = w*2+1
+    # arr = list()
+
+    for station in stations:
+        s = station - w
+        e = station + w 
+        l = s-1-i
+        answer += math.ceil(l/wl)
+        # arr.append([i,l])
+        # print(math.ceil(l/wl))
+        i = e
+    else:
+        if i < n:        
+            l = l-1
+            answer += math.ceil(l/wl)
+            # arr.append([i,l])
+            # print(math.ceil(l/wl))
+    # print(arr)
+
+    return answer
